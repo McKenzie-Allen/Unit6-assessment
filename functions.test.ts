462dcdba376e4cce0613eba7e6484bedbc19c06a
McKenzie Allen <mckenzie.allen144@gmail.com>
2022-02-12 23:33:03 -0700

    2 tests run and pass

--- functions.test.ts
+++ functions.test.ts
@@ -2,4 +2,11 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
     // CODE HERE
+    const array = [1, 2, 3, 4, 5]
+    test('shuffleArray should return an array with the same length',() =>{
+        expect(shuffleArray(array)).toHaveLength(array.length)
+    })
+    test('shuffelArray returs an array that is shuffeled', ()=>{
+        expect(shuffleArray(array)).not.toBe(array)
+    })
 })
